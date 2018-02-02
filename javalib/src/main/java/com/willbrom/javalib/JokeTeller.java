4be819558e4f2fa200aafe3859e0871654317060
Will <alisajid552@yahoo.com>
2018-02-02 20:40:04 +0300

    created JokeTeller class: added static method that returns a joke

---
+++ javalib/src/main/java/com/willbrom/javalib/JokeTeller.java
@@ -0,0 +1,7 @@
+package com.willbrom.javalib;
+
+public class JokeTeller {
+    public static String getJoke() {
+        return "I'm only friends with 25 letters of the alphabet. I don't know Y.";
+    }
+}
